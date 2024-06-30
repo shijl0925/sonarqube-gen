7acf5bdb164d045817c6d58a2a334e8a57a5cb18
shijl0925 <1213970695@qq.com>
2024-06-30 14:59:24 +0800

    支持传入sonarqube网址解析webservice

--- gen/main.go
+++ gen/main.go
@@ -4,9 +4,12 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
-	"io/ioutil"
+	"net/http"
+	"os"
 	"sync"
+	"time"
 )
 
 type Api struct {
@@ -24,19 +27,70 @@ var skippedEndpoints = []string{
 // These fields don't need to be in each request struct
 var skippedRequestFields = []string{}
 
-const input = "gen/webservices.json"
 const packageName = "sonarqube"
 
+const (
+	webservicesUrl     = "/api/webservices/list"
+	includeInternalUrl = "?include_internals=true"
+)
+
+var (
+	host     string
+	internal bool
+	help     bool
+	auth     string
+)
+
 func main() {
-	file, err := ioutil.ReadFile(input)
-	guard(err)
+	var mainFlagsSet = flag.NewFlagSet("", flag.PanicOnError)
+	mainFlagsSet.StringVar(&host, "host", "http://localhost:9000", "SonarQube server")
+	mainFlagsSet.BoolVar(&internal, "internal", false, "generate code for internal methods (default: false)")
+	mainFlagsSet.BoolVar(&help, "help", false, "show usage")
+	mainFlagsSet.StringVar(&auth, "auth", "", "the header Authorization value,example: Basic YWRtaW46YWRtaW4=")
+	mainFlagsSet.Parse(os.Args[1:])
+	if help {
+		mainFlagsSet.Usage()
+		os.Exit(0)
+	}
+
+	apiUrl := host + webservicesUrl
+	if internal {
+		apiUrl += includeInternalUrl
+	}
+
+	req, err := http.NewRequest("GET", apiUrl, nil)
+	if err != nil {
+		fmt.Printf("failed to create request: %v", err)
+		exit(1, err)
+	}
+	if auth != "" {
+		req.Header.Add("Authorization", auth)
+	}
+	client := http.Client{
+		Timeout: 15 * time.Second, // 设置超时时间
+	}
+	resp, err := client.Do(req)
+
+	if resp.StatusCode == 401 {
+		fmt.Println("Authorization failed")
+		os.Exit(1)
+	}
+
+	if err != nil {
+		fmt.Printf("failed to fetch api definitions：%v", err)
+		exit(1, err)
+	}
+	defer resp.Body.Close()
 
 	var api Api
-	err = json.Unmarshal(file, &api)
-	guard(err)
+	if err := json.NewDecoder(resp.Body).Decode(&api); err != nil {
+		fmt.Errorf("could not decode response: %+v", err)
+	}
 
 	wg := &sync.WaitGroup{}
 	for _, service := range api.Services {
+		fmt.Printf("processing service at path %s\n", service.Path)		s := service
+
 		s := service
 
 		wg.Add(1)
